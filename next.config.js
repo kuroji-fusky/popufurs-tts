18cc3100d7f0f78699d87073f5ebd4c1358ec64d
Kerby Keith Aquino <skepfoosky15@gmail.com>
2022-09-03 15:42:47 +0800

    Initial commit
    Move dem shits
    
    trimming the fat

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development'
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+    ]
+  }
+}
+
+module.exports = nextConfig
